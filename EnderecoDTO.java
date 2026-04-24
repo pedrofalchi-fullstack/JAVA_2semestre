e96443d09558e88c6cca1561f82e424a7ae6eeca
pedrofalchi-fullstack <peeh.falchi@gmail.com>
2026-04-24 20:49:08 -0300

    aula API

---
+++ EnderecoDTO.java
@@ -0,0 +1,105 @@
+package Model;
+
+// Data Transfer Object
+public class EnderecoDTO {
+
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String estado;
+	private String regiao;
+	private String ddd;
+	
+	//construtor padrao obrigatorio para o Json
+	public EnderecoDTO() {}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	@Override
+	public String toString() {
+		return "Endereço encontrado: " 
+				+ "\nCEP: "  + cep 
+				+ "\nLogradouro: " + logradouro 
+				+ "\nBairro: " + bairro 
+				+ "\nLocalidade: "
+				+ localidade + "]";
+	}
+	
+	
+
+	
+	
+}
